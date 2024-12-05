7798c0b418b2ae923806be6d34b9e4a926c9e9a0
Andrzej Sokołowski <sokolowski.a95@gmail.com>
2024-12-05 18:23:15 +0100

    finish?

--- todo/taskmanager/urls.py
+++ todo/taskmanager/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path("home", views.main, name="main"),
-    path("test", views.todomanager, name="todomanager"),
+    path("todomanager", views.todomanager, name="todomanager"),
     path("task-list", views.getTaskList, name="getTaskList"),
     path("add", views.add, name="add"),
     path("delete/<int:task_id>", views.delete, name="delete"),
